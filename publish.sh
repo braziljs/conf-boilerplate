b255bcfc784882cba4700fc32bf92fd484549ebd
Zeno Rocha <zno.rocha@gmail.com>
2012-10-12 07:44:15 -0300

    adicionando shell script para publicar o site em gh-pages

---
+++ publish.sh
@@ -0,0 +1,11 @@
+# Install dependencies: npm install .
+# Build the website locally: docpad generate
+# Go to the gh-pages branch: git checkout gh-pages
+# Remove all files under version control: git rm -rf .
+# Move files from folder out to root: mv out/* .
+# Removing out folder: rm -rf out
+# Removing node_modules folder: rm -rf node_modules
+# Commit changes to gh-pages branch: git add . && git commit -m "Regenerate" && git push origin gh-pages
+# Goes back to master branch: git checkout master
+
+npm install . && docpad generate && git checkout gh-pages && git rm -rf . && mv out/* . && rm -rf out && rm -rf node_modules && git add . && git commit -m "Regenerate" && git push origin gh-pages && git checkout master
