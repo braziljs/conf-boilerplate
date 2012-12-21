680be91b9d05e52fdce7b3f4c58eb61be2df4a09
felquis <ofelquis@hotmail.com>
2012-12-21 04:36:59 -0300

    Animate scroll

--- src/files/js/main.js
+++ src/files/js/main.js
@@ -1,5 +1,38 @@
-// 21 Dez, 4:42
+// 21 Dez 2012, 4:42
+;(function ($, window, document, undefined) {
+	var mainFunctions = ({
+		animateScroll : function () {
+			$('.nav-link').on('click', function (event) {
+				var $this = $(this),
+					$htmlBody = $('html, body'),
+					linkTarget = $this.attr('href'),
+					offSetTop;
 
-$(function () {
-	
-});
+			// If not start with #, stop here!
+				if (linkTarget[0] !== '#') { return false }
+
+				event.preventDefault();
+
+			console.log(linkTarget);
+
+			// Get distance of top
+				offSetTop = $(linkTarget).offset().top;
+
+			// Animate the scroll
+				$htmlBody.stop().animate({scrollTop : offSetTop}, function () {
+					location.hash = linkTarget;
+				});
+			});
+		},
+
+		init : function () {
+			var that = this;
+
+			$(function () {
+				console.log('asa');
+				that.animateScroll();
+			});
+		}
+	}).init();
+
+}(jQuery, window, document))
