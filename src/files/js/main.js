8f1349e1d7dbd9e0f114c3e78b2f9d387e4c2d2a
felquis <ofelquis@hotmail.com>
2012-12-22 01:30:00 -0200

    Indentação com 2 espaços

--- src/files/js/main.js
+++ src/files/js/main.js
@@ -1,36 +1,36 @@
 // 21 Dez 2012, 4:42
 
 ;(function ($, window, document, undefined) {
-	var mainFunctions = ({
-		animateScroll : function () {
-			$('.nav-link').on('click', function (event) {
-				var $this = $(this),
-  					$htmlBody = $('html, body'),
-  					linkTarget = $this.attr('href'),
-  					offSetTop;
+  var mainFunctions = ({
+    animateScroll : function () {
+      $('.nav-link').on('click', function (event) {
+        var $this = $(this),
+            $htmlBody = $('html, body'),
+            linkTarget = $this.attr('href'),
+            offSetTop;
 
-			// If not start with #, stop here!
-				if (linkTarget[0] !== '#') { return false }
+      // If not start with #, stop here!
+        if (linkTarget[0] !== '#') { return false }
 
-				event.preventDefault();
+        event.preventDefault();
 
-			// Get distance of top
-				offSetTop = $(linkTarget).offset().top;
+      // Get distance of top
+        offSetTop = $(linkTarget).offset().top;
 
-			// Animate the scroll
-				$htmlBody.stop().animate({scrollTop : offSetTop}, function () {
-					location.hash = linkTarget;
-				});
-			});
-		},
+      // Animate the scroll
+        $htmlBody.stop().animate({scrollTop : offSetTop}, function () {
+          location.hash = linkTarget;
+        });
+      });
+    },
 
-		init : function () {
-			var that = this;
+    init : function () {
+      var that = this;
 
-			$(function () {
-				that.animateScroll();
-			});
-		}
-	}).init();
+      $(function () {
+        that.animateScroll();
+      });
+    }
+  }).init();
 
 }(jQuery, window, document))
