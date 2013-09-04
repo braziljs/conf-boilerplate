bcb434cdbaee66a472f38e04427944fa26041c1c
Felquis Gimenes <frgformenton@gmail.com>
2013-09-04 01:22:19 -0300

    Add dynamic content in call to action button

--- docpad.js
+++ docpad.js
@@ -10,11 +10,19 @@ module.exports = {
       name: "Conference name",
       description: "Conference description",
       date: "November 15",
+      // If your event is free, just comment this line
       price: "$100",
       venue: "Coco Bongo",
       address: "Boulevard Kukulcan, 30",
       city: "Cancún",
       state: "Quintana"
+    },
+
+    // The Call To Action button at the header,
+    // If you don't want this, just remove the callToAction property.
+    callToAction: {
+        text: "Register now!",
+        link: "http://eventick.com.br"
     },
 
     // Site info
