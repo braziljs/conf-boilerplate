cd814fac615a36c4a192e1d40b0efa2c77b98d21
Felquis Gimenes <frgformenton@gmail.com>
2014-07-05 12:37:07 -0300

    Reindent to spaces and remove trailing spaces

--- src/documents/themes/yellow-swan/js/main.js
+++ src/documents/themes/yellow-swan/js/main.js
@@ -40,7 +40,7 @@
     };
 
     conf.map.createMarker = function () {
-        
+
         conf.map.address = conf.map.element.attr('data-address');
 
         conf.map.geocoder.geocode({ 'address': conf.map.address}, function (results, status) {
@@ -86,7 +86,7 @@
         var $link = $(link),
             href = $link.attr('href'),
             offSetTop = $(href).offset().top;
-        
+
         conf.menu.document.finish().animate({scrollTop : offSetTop}, conf.menu.animationSpeed, function () {
             location.hash = href;
         });
