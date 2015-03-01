248ae5b0b2106861d19b2f5b9c9cbc0dc4e65702
Beto Muniz <contato@betomuniz.com>
2015-03-01 00:19:10 -0300

    Using inline data for data binding in favor of the current implementation.

--- metalsmith.js
+++ metalsmith.js
@@ -4,7 +4,7 @@ var Metalsmith  = require('metalsmith');
 var markdown    = require('metalsmith-markdown');
 var templates   = require('metalsmith-templates');
 var less        = require('metalsmith-less');
-var metadata    = require('metalsmith-metadata');
+var metaobject    = require('metalsmith-metaobject');
 var Handlebars  = require('handlebars');
 
 console.log('Building ' + __dirname + '..');
@@ -14,14 +14,170 @@ console.log('Building ' + __dirname + '..');
  */
 
 var templateData = {
-  conf: "data/conf.json",
-  site: "data/site.json",
-  sections: "data/sections.json",
-  partners: "data/partners.json",
-  schedule: "data/schedule.json",
-  sponsors: "data/sponsors.json",
-  misc: "data/misc.json"
-}
+  "conf": {
+    "name": "Conference name",
+    "description": "Conference description",
+    "date": "November 15",
+    "price": "$100",
+    "venue": "Coco Bongo",
+    "address": "Boulevard Kukulcan, 30",
+    "city": "Cancún",
+    "state": "Quintana"
+  },
+
+  "callToAction": {
+    "text": "Register now!",
+    "link": "http://eventick.com.br"
+  },
+  "forkButton": {
+    "repository": "https://github.com/braziljs/conf-boilerplate"
+  },
+  "site": {
+    "theme": "yellow-swan",
+    "url": "http://braziljs.github.io/conf-boilerplate",
+    "googleanalytics": "UA-33656081-1"
+  },
+  "sections": [
+    {
+      "slug": "about",
+      "label": "About"
+    },
+    {
+      "slug": "location",
+      "label": "Location"
+    },
+    {
+      "slug": "speakers",
+      "label": "Speakers"
+    },
+    {
+      "slug": "schedule",
+      "label": "Schedule"
+    },
+    {
+      "slug": "sponsors",
+      "label": "Sponsors"
+    },
+    {
+      "slug": "partners",
+      "label": "Partners"
+    },
+    {
+      "slug": "contact",
+      "label": "Contact"
+    }
+  ],
+  "schedule": [
+    {
+      "name": "Check-in / Breakfast",
+      "time": "9h00"
+    }, {
+      "name": "Linus Torvalds",
+      "photo": "themes/yellow-swan/img/speaker.jpg",
+      "bio": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+      "company": "Linux Foundation",
+      "link": {
+        "href": "http://twitter.com/linus",
+        "text": "@linus"
+      },
+      "presentation": {
+        "title": "Digging into a Linux Kernel",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        "time": "10h00"
+      }
+    }, {
+      "name": "Bill Gates",
+      "photo": "themes/yellow-swan/img/speaker.jpg",
+      "bio": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+      "company": "Microsoft",
+      "link": {
+        "href": "http://github.com/billy95",
+        "text": "@billy95"
+      },
+      "presentation": {
+        "title": "Introducing Windows 12",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        "time": "11h00"
+      }
+    }, {
+      "name": "Lunch",
+      "time": "12h00"
+    }, {
+      "name": "Chuck Norris",
+      "photo": "themes/yellow-swan/img/speaker.jpg",
+      "bio": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+      "company": "Delta Command",
+      "link": {
+        "href": "http://twitter.com/littlechuck",
+        "text": "@littlechuck"
+      },
+      "presentation": {
+        "title": "How to kill a elephant with one finger",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        "time": "13h00"
+      }
+    }, {
+      "name": "Steve Jobs",
+      "photo": "themes/yellow-swan/img/speaker.jpg",
+      "bio": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+      "company": "Apple, Inc.",
+      "link": {
+        "href": "http://github.com/stevie",
+        "text": "@stevie"
+      },
+      "presentation": {
+        "title": "Presenting iPad",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        "time": "14h00"
+      }
+    }, {
+      "name": "Coffee-break",
+      "time": "15h00"
+    }, {
+      "name": "Mark Zuckerberg",
+      "photo": "themes/yellow-swan/img/speaker.jpg",
+      "bio": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+      "company": "Facebook",
+      "link": {
+        "href": "http://twitter.com/zuck",
+        "text": "@zuck"
+      },
+      "presentation": {
+        "title": "Revealing Facebook Secrets",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        "time": "16h00"
+      }
+    }, {
+      "name": "Steve Wozniak",
+      "photo": "themes/yellow-swan/img/speaker.jpg",
+      "bio": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+      "company": "Apple, Inc.",
+      "link": {
+        "href": "http://twitter.com/woz",
+        "text": "@woz"
+      },
+      "presentation": {
+        "title": "Why do I prefer Android over iPhone",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        "time": "17h00"
+      }
+    }
+  ],
+  "sponsors": [
+    {
+      "name": "Eventick",
+      "logo": "themes/yellow-swan/img/sponsor.png",
+      "url": "http://eventick.com.br"
+    }
+  ],
+  "partners": [
+    {
+      "name": "BrazilJS",
+      "logo": "themes/yellow-swan/img/partner.png",
+      "url": "http://braziljs.org"
+    }
+  ]
+};
 
 /**
  * Build.
@@ -32,7 +188,7 @@ Metalsmith(__dirname)
   .source('src/documents')
   .destination('out')
   .use(markdown())
-  .use(metadata(templateData))
+  .use(metaobject(templateData))
   .use(less())
   .use(templates({
     engine: 'handlebars',
@@ -65,6 +221,10 @@ partialsDirs.forEach(function (partialsDir) {
 /**
  * Helpers.
  */
+
+Handlebars.registerHelper('toUpperCase', function(ctx, options) {
+  return ctx.toUpperCase();
+});
 
 Handlebars.registerHelper('partial', function(name, ctx, options) {
   var template = Handlebars.compile(Handlebars.partials[name]);
