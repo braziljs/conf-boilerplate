d5a14d6c62261c75a544776c46314a15d3ade0a1
Zeno Rocha <hi@zenorocha.com>
2016-05-05 23:10:09 -0700

    Formats code to ES6

--- metalsmith.js
+++ metalsmith.js
@@ -1,24 +1,23 @@
-var start = process.hrtime();
-var task = process.argv[2];
+const start = process.hrtime();
+const task  = process.argv[2];
 
-var metalsmith = require('metalsmith')(__dirname);
-var plugins    = require('load-metalsmith-plugins')();
-var conf       = require('./conference');
-var chalk      = require('chalk');
-var ghpages    = require('gh-pages');
-var prettytime = require('pretty-hrtime');
-
-/* Config
+/* Dependencies
    ========================================================================== */
 
-metalsmith.source('src/documents').destination('out').clean(true);
+const conf       = require('./conference');
+const chalk      = require('chalk');
+const ghpages    = require('gh-pages');
+const plugins    = require('load-metalsmith-plugins')();
+const prettytime = require('pretty-hrtime');
+const metalsmith = require('metalsmith')(__dirname);
 
-/* Plugins
+/* Configuration
    ========================================================================== */
-metalsmith
-  .use(plugins.less())
-  .use(plugins.metaobject(conf))
-  .use(plugins.layouts({
+
+const config = {
+  source: 'src/documents',
+  destination: 'out',
+  layouts: {
     directory: 'src/layouts',
     engine: 'handlebars',
     partials: {
@@ -32,48 +31,63 @@ metalsmith
       speakers : '../partials/section/speakers',
       sponsors : '../partials/section/sponsors'
     }
-  }))
-  .use(plugins.inPlace({
+  },
+  inPlace: {
     directory: 'src/layouts',
     engine: 'handlebars'
-  }));
+  },
+  serve: {
+    port: 9778,
+    verbose: true
+  },
+  watch: {
+    paths: {
+      "${source}/**/*": true,
+      "src/layouts/**/*": "**/*.html",
+      "src/partials/**/*": "**/*.html"
+    }
+  }
+};
 
-/* Watch
-   ========================================================================== */
-
-if (task === 'watch') {
-  metalsmith
-    .use(plugins.serve({
-      port: 9778,
-      verbose: true
-    }))
-    .use(plugins.watch({
-      paths: {
-        "${source}/**/*": true,
-        "src/layouts/**/*": "**/*.html",
-        "src/partials/**/*": "**/*.html"
-      }
-    }));
-}
-
-/* Build & Deploy
+/* Pipeline
    ========================================================================== */
 
 metalsmith
-  .build(function(err) {
-    if (err) {
-      throw err;
-    }
-    else {
-      if (task === 'generate') {
-        var end = prettytime(process.hrtime(start));
-        console.log('> done in ' + chalk.green(end));
-      }
+  .source(config.source)
+  .destination(config.destination)
+  .use(plugins.less())
+  .use(plugins.metaobject(conf))
+  .use(plugins.layouts(config.layouts))
+  .use(plugins.inPlace(config.inPlace));
 
-      if (task === 'deploy') {
-        ghpages.publish('out', function(err) {
-          if (err) throw err;
-        });
-      }
-    }
-  });
+if (task === 'watch') {
+  metalsmith
+    .use(plugins.serve(config.serve))
+    .use(plugins.watch(config.watch));
+}
+
+/* Generate
+   ========================================================================== */
+
+metalsmith.build((err) => {
+  if (err) throw err;
+  else buildCompleted();
+});
+
+const buildCompleted = () => {
+  if (task === 'generate') {
+    buildDuration();
+  }
+
+  if (task === 'deploy') {
+    ghpages.publish(config.destination, (err) => {
+      if (err) throw err;
+      else buildDuration();
+    });
+  }
+}
+
+const buildDuration = () => {
+  const end = prettytime(process.hrtime(start));
+  console.log(`> done in ${chalk.green(end)}`);
+}
